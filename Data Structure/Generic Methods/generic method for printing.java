cc15e8df76fc94a0d7c8a5b78ec04bbbcc32a2fc
Rishi Yadav <rahulrishiyadav1994@gmail.com>
2024-09-27 04:39:01 +0530

    Update generic method for printing.java

--- Data Structure/Generic Methods/generic method for printing.java
+++ Data Structure/Generic Methods/generic method for printing.java
@@ -1,12 +1,16 @@
-class printgen{
-  // Defining a generic method to print any data type
-  void genericPrint(T t){
-    System.out.println(t);
-  }
-  public static void main(String[] args){
-    printgen aObj; // creating an object of the class printgen
-    aObj.genericPrint(101); // Calling generic method with int argument
-    aObj.genericPrint("Joy with java"); // calling generic method with string
-    aObj.genericPrint(3.1412343); // calling generic method with double
-  }
-}  
+class printgen<T> {
+    // Defining a generic method to print any data type
+    void genericPrint(T t) {
+        System.out.println(t);
+    }
+
+    public static void main(String[] args) {
+        // Creating an object of the class printgen with Object type
+        printgen<Object> aObj = new printgen<>(); 
+
+        // Calling the generic method with different types of arguments
+        aObj.genericPrint(101);               // int argument
+        aObj.genericPrint("Joy with java");   // String argument
+        aObj.genericPrint(3.1412343);         // double argument
+    }
+}
